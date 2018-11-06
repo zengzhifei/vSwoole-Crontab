9a74c21d0b7a3d6ff65b7e46d236300c9150b865
zengzhifei <zengzhifei@ctvit.com.cn>
2018-11-06 17:16:24 +0800

    vSwoole-Crontab

---
+++ configs/const.php
@@ -0,0 +1,16 @@
+<?php
+// +----------------------------------------------------------------------+
+// | VSwoole FrameWork                                                    |
+// +----------------------------------------------------------------------+
+// | Not Decline To Shoulder a Responsibility                             |
+// +----------------------------------------------------------------------+
+// | zengzhifei@outlook.com                                               |                  
+// +----------------------------------------------------------------------+
+
+/**
+ * 服务+别名注册
+ * defined('VSWOOLE_SERVERNAME_SERVER') or define('VSWOOLE_SERVERNAME_SERVER', 'Swoole_Servername_Server') and define('Servername', VSWOOLE_SERVERNAME_SERVER);
+ */
+
+//Crontab服务
+defined('VSWOOLE_CRONTAB_SERVER') or define('VSWOOLE_CRONTAB_SERVER', 'Swoole_Crontab_Server') and define('Crontab', VSWOOLE_CRONTAB_SERVER);
